886ad66a194ad4993648f83fe7b4e7bb0d4d346d
jennaasun <jls9966@nyu.edu>
2024-05-09 15:13:12 +0800

    laoye

--- project-b/js/page8.js
+++ project-b/js/page8.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
 
+// addEventListener(type, listener, capture)
+document.addEventListener('DOMContentLoaded', function () {
+// 'click'
     var triggerImage = document.querySelector('.red-cd-button-img');
 
     var popup = document.getElementById('popup-cd');
 
-    triggerImage.addEventListener('click', function() {
-        popup.style.display = 'block';
+    triggerImage.addEventListener('click', function () {
+        popup.window.display = 'block';
     });
 
     var closeButton = document.querySelector('.close-popup');
 
-    closeButton.addEventListener('click', function(){
-        popup.style.display = 'none';
+    closeButton.addEventListener('click', function () {
+        popup.window.display = 'none';
     });
 });
